7332f730c19fdf2423f7ff07eb58c1c3ffc5d70c
Juneid Teklu <te.juneid@gmail.com>
2025-02-07 13:08:03 -0600

    Create functions that get the word and character count of text

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        
+
+        word_count = countWords(file_contents)
+        character_counts = countChars(file_contents)
+        print(len(word_count))
+        print(character_counts)
+        
+
+
+def countWords(file):
+    return file.split()
+
+def countChars(file):
+    list_of_chars = list(file)
+    char_dict = {}
+
+    for i in list_of_chars:
+        lower_char = i.lower()
+        if lower_char in char_dict:
+            char_dict[lower_char] += 1
+        else:
+            char_dict[lower_char] = 1
+    return char_dict
+    
+    
+
+
+
+main()
+
